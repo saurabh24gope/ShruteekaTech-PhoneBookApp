bf66251833eb02e1cc76c0d513a39966fbfa6453
saurabh24gope <saurabhgope@gmail.com>
2022-12-06 11:15:31 +0530

    SPHON-1 create spring boot project-3

---
+++ src/main/java/com/shrueekatech/ShrueeKaTechPhneBookApp/ShrueeKaTechPhneBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.shrueekatech.ShrueeKaTechPhneBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrueeKaTechPhneBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShrueeKaTechPhneBookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrueekatech/ShrueeKaTechPhneBookApp/ShrueeKaTechPhneBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrueekatech.ShrueeKaTechPhneBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShrueeKaTechPhneBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
